60ff4e233ca42a8874dc7db87d14bd2cb56943c7
worldzhy <worldzhy@126.com>
2025-06-27 10:48:00 +0800

    feat: add google-drive

--- googleapis.module.ts
+++ googleapis.module.ts
@@ -2,10 +2,24 @@ import {Global, Module} from '@nestjs/common';
 import {GoogleFormService} from './google-form.service';
 import {GoogleSheetService} from './google-sheet.service';
 import {GoogleTimezoneService} from './google-timezone.service';
+import {GoogleDriveService} from './google-drive/google-drive.service';
+import {GoogleDrivePermissionService} from './google-drive/google-drive-permission.service';
 
 @Global()
 @Module({
-  providers: [GoogleFormService, GoogleSheetService, GoogleTimezoneService],
-  exports: [GoogleFormService, GoogleSheetService, GoogleTimezoneService],
+  providers: [
+    GoogleDriveService,
+    GoogleDrivePermissionService,
+    GoogleFormService,
+    GoogleSheetService,
+    GoogleTimezoneService,
+  ],
+  exports: [
+    GoogleDriveService,
+    GoogleDrivePermissionService,
+    GoogleFormService,
+    GoogleSheetService,
+    GoogleTimezoneService,
+  ],
 })
 export class GoogleAPIsModule {}
